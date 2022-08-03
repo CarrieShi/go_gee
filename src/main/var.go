15d6029b27d9a42d9ef70dd20245d8e959aa02e3
CarrieShi <hichunlishi@sina.cn>
2022-08-03 22:36:14 +0800

    var

---
+++ src/main/var.go
@@ -0,0 +1,23 @@
+package main
+
+var aa = "test"
+
+var (
+	p1 string = "is string ah"
+	p2 int32  = 16
+)
+
+const EARTH = "earth"
+
+func main() {
+	println(aa)
+	aa := "test cover"
+	println(aa)
+
+	println(p1)
+	println(p2)
+
+	var defaultWhat int
+	println(defaultWhat)
+
+}
