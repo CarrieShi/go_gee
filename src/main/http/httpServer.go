e3bf44336c3596d6a16dec0e8ba504541fd169e8
CarrieShi <hichunlishi@sina.cn>
2022-09-04 18:07:05 +0800

    type

--- src/main/http/httpServer.go
+++ src/main/http/httpServer.go
@@ -1,1 +1,17 @@
 package main
+
+import "net/http"
+
+type Server interface {
+	// Route 设定路由，命中路由会执行 handlerFunc 的代码
+	Route(pattern string, handleFunc http.HandlerFunc)
+
+	// Start 启动服务
+	Start(address string) error
+}
+
+type sdkHttpServer struct {
+	Name string
+}
+
+type Header map[string][]string
