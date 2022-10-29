1578e89606027c2fcc62d0002adb0d81cbd03029
CarrieShi <hichunlishi@sina.cn>
2022-10-29 20:51:20 +0800

    http server

--- src/main/http/httpServer.go
+++ src/main/http/httpServer.go
@@ -10,6 +10,18 @@ type Server interface {
 	Start(address string) error
 }
 
+// sdkHttpServer 基于 http 实现
 type sdkHttpServer struct {
 	Name string
+}
+
+// Route 注册路由
+func (s *sdkHttpServer) Route(pattern string, handleFunc http.HandlerFunc) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *sdkHttpServer) Start(address string) error {
+	//TODO implement me
+	panic("implement me")
 }
