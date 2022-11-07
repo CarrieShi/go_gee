ff33fbab26608019571641432b4fbb4c95e23a8c
CarrieShi <hichunlishi@sina.cn>
2022-11-07 23:19:52 +0800

    closure delay

--- src/main/basic/closure.go
+++ src/main/basic/closure.go
@@ -10,4 +10,33 @@ func main() {
 		fmt.Printf("say hi to %s \n", name)
 	}
 	sayHiTo()
+
+	fmt.Println(ReturnClosure("JIM")())
+
+	Delay()
+}
+
+func ReturnClosure(name string) func() string {
+	return func() string {
+		return "hello, " + name
+	}
+}
+
+func Delay() {
+	// 延迟绑定
+	fns := make([]func(), 0, 10)
+	for i := 0; i < 10; i++ {
+		fns = append(fns, func() {
+			fmt.Printf("hello, this is %d \n", i)
+		})
+	}
+
+	// 执行匿名函数
+	// 结果全是 hello, this is 10 ==> 最近一次的 i
+	// 而不是
+	// hello, this is 1
+	// hello, this is ...
+	for _, fn := range fns {
+		fn()
+	}
 }
